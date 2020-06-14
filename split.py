dc53ff64f65d12a8c12534b4c359a2a9a4faa36d
Elias Summermatter <elias.summermatter@seccom.ch>
2020-06-14 18:15:57 +0200

    First working version enc/dec

--- split.py
+++ split.py
@@ -5,17 +5,21 @@ import hashlib
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from base64 import b64encode
+from base64 import b64decode
 import os
 from os import path
 import shutil
 import py7zr
 import sys
+import math
+
 
 argon_time=200
 argon_memory=100  #100000
 distribution=3
 redundancy=2
 passphrase=""
+salt=""
 tmpPath="./tmp/"
 specFile="spec"
 
@@ -37,14 +41,12 @@ def log(level,message):
         exit(99)
 
 def split(data,split):
-    chucksize=int(getChucksize(data))
+    chucksize=math.ceil(getChucksize(data))
     return data[split * chucksize: (split + 1) * chucksize]
 
 def writeChuck(data,distCtr,splitCtr,extension):
     # Write file
-    file = open(tmpPath + 'P' + str(distCtr) + str(splitCtr) + '.' + extension , 'wb')
-    file.write(data)
-    file.close()
+    saveFile(tmpPath + 'P' + str(distCtr) + str(splitCtr) + '.' + extension, data)
     hash = generateHashFromData(data)
     appendFile(tmpPath + specFile, str(distCtr) + ";" + str(splitCtr) + ";" + extension + ";" + str(hash) + "\n")
 
@@ -59,8 +61,9 @@ def appendFile(file ,data):
 
 def basicConfig(salt):
     log("i","Writing config File....")
+    filename, file_extension = os.path.splitext(filePath[0])
     f=open(tmpPath + specFile, "a+")
-    f.write(str(distribution)+ ";" + str(redundancy) + ";" + str(salt) + ";" + str(argon_time) + ";" + str(argon_memory) + "\n")
+    f.write(str(distribution)+ ";" + str(redundancy) + ";" + str(salt) + ";" + str(argon_time) + ";" + str(argon_memory) + ";" + file_extension + "\n")
     f.close()
 
 def calcuateDistribution(part):
@@ -76,7 +79,7 @@ def cleanUpWorking():
 def generateRandumSalt():
     return b64encode(get_random_bytes(15)).decode("utf-8")
 
-def generateKeys(key,salt, time, memory):
+def generateKeys(key,salt, time, memory, distribution):
     log("i","Generating keys....")
     keys=[]
     keys.append(argon2.argon2_hash(key, salt, t=time, m=memory))
@@ -107,6 +110,7 @@ def createSplits():
             ident="P" + str(l) + str(copied[l]) + "."
             shutil.move(tmpPath + ident + "cipher", dist_path)
             shutil.move(tmpPath + ident + "tag" , dist_path)
+            shutil.move(tmpPath + ident + "nonce" , dist_path)
 
         # Copy  spec config
         shutil.copy(tmpPath + specFile, dist_path)
@@ -119,8 +123,8 @@ def createSplits():
         archive.close()
         os.chdir(root_path)
 
-def readSecret(filePath):
-    log("i","Reading secret.")
+def readFile(filePath):
+    log("i","Reading [" + filePath + "]...")
     file = open(filePath, "rb")
     data=file.read()
     file.close()
@@ -139,22 +143,132 @@ def encryptSecret(keys, data, splitsCount):
         for l in range(0,splitsCount):
             writeChuck(split(b64encode(ciphertext),l), distribution_ctr, l, "cipher")
             writeChuck(split(b64encode(tag),l), distribution_ctr, l, "tag" )
+            writeChuck(split(b64encode(cipher.nonce),l), distribution_ctr, l, "nonce" )
 
         # Increase Distribution ctr
         distribution_ctr+=1
 def doesFileExist(file_path):
     return path.exists(file_path)
 
-def main_encryption():
-    # Main split/encryption Process
+def makeEnvReady():
     cleanUpWorking()
     os.mkdir(tmpPath)
+
+def unpackFiles():
+    for file in filePath:
+        log("i", "Unpacking file [" + file + "]")
+        try:
+            archive = py7zr.SevenZipFile(file, mode='r')
+            archive.extractall(path=tmpPath)
+            archive.close()
+        except:
+            log("e", "Error while extracting archive [" + file + "]")
+
+def readSpecFile():
+    validConfig=False
+    distribution=""
+    redundancy= ""
+    salt=""
+    argon_time=""
+    argon_memory=""
+    files={}
+    if doesFileExist(tmpPath + specFile):
+        log("i", "Reading spec file")
+        f = open(tmpPath + specFile, "r")
+        for line in f:
+            array=line.rstrip().split(';')
+
+            if not validConfig:
+                distribution=int(array[0])
+                redundancy=int(array[1])
+                salt=array[2]
+                argon_time=int(array[3])
+                argon_memory=int(array[4])
+                fileEnd=array[5]
+                validConfig=True
+            else:
+                filename = "P" + array[0] + array[1] + "." + array[2]
+                files[filename]=array[3]
+        return files, distribution, redundancy, salt, argon_time, argon_memory, fileEnd
+    else:
+        cleanUpWorking()
+        log("e", "No spec file found in unpacked data!")
+
+def validateFiles(files):
+    returnFiles={}
+    for filename in files:
+        if doesFileExist(tmpPath + filename):
+            log("i", "Validating part [" + filename + "]")
+            data = readFile(tmpPath + filename)
+            if generateHashFromData(data) == files[filename]:
+                log("i","Part [" + filename + "] is valid")
+                returnFiles[filename] = data
+            else:
+                log("e", "Part [" + filename + "] is invalid. exciting...")
+        else:
+            log("i", "Skipping missing Part [" + filename + "]")
+    return returnFiles
+
+def findValidSplit(files,redundancy):
+    lib={}
+    for filename in files:
+        splitId=filename[1:2]
+        if splitId not in lib.keys():
+            lib[splitId] = 1
+        else:
+            lib[splitId]=lib[splitId] + 1
+        if lib[splitId] >= (redundancy * 3):
+            return splitId
+    return False
+
+def mergeSplits(files,splitId,redundancy):
+    tag=""
+    cipher=""
+    nonce=""
+    for i in range(0,redundancy):
+        filename="P" + str(splitId) + str(i)
+        tag+=str(files[filename + ".tag"].decode("utf-8"))
+        cipher+=str(files[filename + ".cipher"].decode("utf-8"))
+        nonce+=str(files[filename + ".nonce"].decode("utf-8"))
+    return b64decode(nonce), b64decode(cipher), b64decode(tag)
+
+def decryptSecret(keys,splitId,ciphertext,tag,nonce):
+    log("i", "Decrypting data...")
+    try:
+        cipher = AES.new(keys[int(splitId)], AES.MODE_GCM, nonce=nonce)
+        return cipher.decrypt_and_verify(ciphertext, tag)
+    except:
+        log("e", "Some error while decryption of secret occurred! exciting...")
+
+def saveFile(path, data):
+    log("i", "Save file as" + path)
+    file = open(path , 'wb')
+    file.write(data)
+    file.close()
+
+def main_encryption():
+    # Main split/encryption Process
+    makeEnvReady()
     salt=generateRandumSalt()
     basicConfig(salt)
-    keys = generateKeys(passphrase, salt, argon_time, argon_memory)
-    data=readSecret(filePath[0])
+    keys = generateKeys(passphrase, salt, argon_time, argon_memory,distribution)
+    data=readFile(filePath[0])
     encryptSecret(keys,data,redundancy)
     createSplits()
+    cleanUpWorking()
+
+def main_decryption():
+    makeEnvReady()
+    unpackFiles()
+    files, distribution, redundancy, salt, argon_time, argon_memory, fileExtension = readSpecFile()
+    files = validateFiles(files)
+    completeSplitId = findValidSplit(files, redundancy)
+    if not completeSplitId:
+        log("e", "Not enough valid splits found to restore secret!")
+    nonce, cipher, tag = mergeSplits(files,completeSplitId,redundancy)
+    keys = generateKeys(passphrase,salt,argon_time,argon_memory, distribution)
+    cleartext = decryptSecret(keys,completeSplitId,cipher,tag,nonce)
+    saveFile("restored-file" + fileExtension, cleartext)
     cleanUpWorking()
 
 
@@ -190,6 +304,15 @@ for argCtr in range(1,argLen):
         argon_memory=int(next_arg)
         log("i","Setting argon memory setting to " + next_arg + "kb")
         jumpNextArg=True
+    elif arg == "--distribution":
+        distribution=int(next_arg)
+        log("i","Setting distribution parts to " + next_arg + " archives")
+        jumpNextArg=True
+    elif arg == "--redundancy":
+        redundancy=int(next_arg)
+        log("i","Setting redundancy to " + next_arg + " archives")
+        jumpNextArg=True
+
     else:
         if mode == "":
             comandlineHelp("Missing mode")
@@ -212,14 +335,13 @@ if len(filePath) == 0:
     comandlineHelp("No file(s) to enc/dec given")
 
 if passphrase == "":
-    log("w", "No passphrase given, so files are only protected through split!")
-
+    txt=""
+    if mode == "e":
+        txt=", so files are only protected through split!"
+    log("w", "No passphrase given with the option -p" + txt)
 
 # Modes
 if mode == "e":
     main_encryption()
-
-
-
-
-
+else:
+    main_decryption()
